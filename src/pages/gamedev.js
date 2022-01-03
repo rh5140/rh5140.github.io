b9c5296b5f9e3da2f0085e91a744ff099a8d9a5f
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2022-01-02 17:09:08 -0800

    More detailed alt text on gamedev page

--- src/pages/gamedev.js
+++ src/pages/gamedev.js
@@ -14,6 +14,8 @@ const GrabbSpan =
   <p>Play the game on <a href="https://raddishradish.itch.io/grabb-grab-grub">itch.io</a> and check out the <a href="https://github.com/rh5140/chameleon-game">GitHub repo</a>!</p>
 </span>
 
+const GrabbAlt = "Screenshot of game with chameleon sitting on a branch with a caught bug on the end of its tongue. There are six other flying bugs on screen, and two concentric pink circles represent the cursor. The top left corner reads 'Time remaining: 00:23' and the top right reads '5 of 30 caught'. The background is a light blue and green watercolor texture."
+
 const BreadySpan = 
 <span>
   <p>A boss battle game developed with a team of eight for <a href="https://itch.io/jam/studiosnowjam">ACM Studio Snowjam 2021</a>. Revamp of <a href="https://aaisara12.itch.io/mundaydreaming">mundaydreaming</a>, from which we reused art assets and my <a href="https://musescore.com/user/39562343/scores/7325906/s/HwmVT8">background music</a>.</p>
@@ -22,8 +24,10 @@ const BreadySpan =
      I worked a little bit on the UI layout and drew the toaster boss, bread, miscellaneous art assets used in the UI, and the dialogue box in Adobe Photoshop.
      </p>
 
-  <p>Play the game on <a href="https://brandmuffin.itch.io/cranky">itch.io</a>!</p>
+  <p>Play the game on <a href="https://aaisara12.itch.io/breadyornot">itch.io</a>!</p>
 </span>
+
+const BreadyAlt = "Screenshot of game with man in office attire running and pointing at a toaster creature with four legs. Health bars are displayed below each character, with the man at full health and toaster at approximately 70% health. A computer terminal on the top with the name 'Norman' reads '> First overtime, then welcoming the new year alone, and now *this*!? This millennium is off to a crumby start...' The background is an illustration of an office."
 
 const CrankySpan = 
 <span>
@@ -35,8 +39,10 @@ const CrankySpan =
     writing all the dialogue in the game and compositing the Unity scenes such that the narrative would flow properly.
   </p>
 
-  <p>Play the game on <a href="https://aaisara12.itch.io/breadyornot">itch.io</a>!</p>
+  <p>Play the game on <a href="https://brandmuffin.itch.io/cranky">itch.io</a>!</p>
 </span>
+
+const CrankyAlt = "Three black and white drawings of an old man wearing a newsboy hat, vest over long-sleeve shirt, long pants, loafers, and holding a cane at different frames of a walking animation underneath the title 'Cranky.' on a white background."
 
 // const HeroSpan =
 // <span>
@@ -60,6 +66,8 @@ const SpaceSpan =
   <p>Play the game on <a href="https://store.steampowered.com/app/1430240/The_Space_in_Between/">Steam</a> (PC only) and <a href="https://volcanogirl.itch.io/the-space-in-between">itch.io</a> (any platform)!</p>
 </span>
 
+const SpaceAlt = "A long-haired Asian girl looking at a constellation she holds in her hand and a boy facing away stand back to back, with the text 'THE SPACE IN BETWEEN' in pale blue to the left. The background is a painterly illustration of the Northern Lights, heavily featuring the colors pink, blue, and teal."
+
 const RamenSpan = 
 <span>
 <p>A visual novel developed with a team of six for <a href="https://itch.io/c/1740406/summer-game-jam-2020-past-present-and-future">ACM Studio's Summer Game Jam 2020</a>.</p>
@@ -72,21 +80,23 @@ const RamenSpan =
 <p>Play the game on <a href="https://volcanogirl.itch.io/ramen-for-two">itch.io</a>!</p>
 </span>
 
+const RamenAlt = "A pixel drawing of an Asian ramen chef in front of a menu and a table with two blue bowls."
+
 const GamedevPage = () => {
   return (
     <Layout pageTitle="Game development">
       <p>Check out all the games I've worked on my <a href="https://raddishradish.itch.io/">itch.io</a> (raddishradish)!</p>
-      <InfoCard image={GrabbScreenshot} alt="Screenshot of game with chameleon shooting tongue at bugs to increase points" text={GrabbSpan} title="Grabb Grab Grub!!"></InfoCard>
+      <InfoCard image={GrabbScreenshot} alt={GrabbAlt} text={GrabbSpan} title="Grabb Grab Grub!!"></InfoCard>
       <p></p>
-      <InfoCard image="https://img.itch.zone/aW1hZ2UvMTMyMjQ1Ny83NjkwMTM5LnBuZw==/original/g6FMif.png" alt="Screenshot of game with man running and pointing at a toaster" text={BreadySpan} title="Bready or Not!"></InfoCard>
+      <InfoCard image="https://img.itch.zone/aW1hZ2UvMTMyMjQ1Ny83NjkwMTM5LnBuZw==/original/g6FMif.png" alt={BreadyAlt} text={BreadySpan} title="Bready or Not!"></InfoCard>
       <p></p>
-      <InfoCard image="https://img.itch.zone/aW1nLzY2NDgyNDYucG5n/original/b6Aqmq.png" alt="Drawing of old man walking underneath the title 'Cranky.'" text={CrankySpan} title="Cranky."></InfoCard>
+      <InfoCard image="https://img.itch.zone/aW1nLzY2NDgyNDYucG5n/original/b6Aqmq.png" alt={CrankyAlt} text={CrankySpan} title="Cranky."></InfoCard>
       {/* <p></p>
       <InfoCard image="https://img.itch.zone/aW1nLzYwNjk3NzMuZ2lm/original/kCbXZG.gif" alt="Six colorful characters standing under the text 'I NEED A HERO!'" text={HeroSpan} title="I Need a Hero! A Super-Powered Dating Adventure!"></InfoCard> */}
       <p></p>
-      <InfoCard image="https://img.itch.zone/aW1nLzQxNzcyMjcucG5n/original/8%2BcjzL.png" alt="A girl looking at a constellation she holds in her hand and a boy facing away stand back to back, with the text 'THE SPACE IN BETWEEN' to the left." text={SpaceSpan} title="The Space in Between"></InfoCard>
+      <InfoCard image="https://img.itch.zone/aW1nLzQxNzcyMjcucG5n/original/8%2BcjzL.png" alt={SpaceAlt} text={SpaceSpan} title="The Space in Between"></InfoCard>
       <p></p>
-      <InfoCard image="https://img.itch.zone/aW1hZ2UvNzUyNjI0LzQyMDMyNDUucG5n/original/Ut7t4z.png" alt="A pixel drawing of a ramen chef in front of a menu and a table with two blue bowls." text={RamenSpan} title="Ramen for Two"></InfoCard>
+      <InfoCard image="https://img.itch.zone/aW1hZ2UvNzUyNjI0LzQyMDMyNDUucG5n/original/Ut7t4z.png" alt={RamenAlt} text={RamenSpan} title="Ramen for Two"></InfoCard>
     </Layout>
   )
 }
