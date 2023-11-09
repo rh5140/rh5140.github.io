41d71122acf506673a75524336fbffdd1bc3f2ef
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2023-11-08 21:05:00 -0800

    improve wording a bit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
 import * as React from 'react'
 import InfoCard from '../components/info-card'
 import Layout from '../components/layout'
-import SMLevelDesign from '../images/SuperMarioLevelDesign.png'
 import Flytrap from '../images/debugger.gif'
 import GrabbScreenshot from '../images/grabb.gif'
 import ScribbleRushScreenshot from '../images/scribble.gif'
+import SMLevelDesign from '../images/smlevel.png'
 import SonderingLogo from '../images/sonderinglogo.png'
 
 const GoldLiningSpan =
 <span>
   A narrative platformer featuring a Chinese American protagonist. Currently in development by <a href="https://twitter.com/sonderingstudio">Sondering Studio</a>, an indie studio partnered with Xbox.
 
-  <p>As the only level designer on the team, I worked on 4 levels to convey emotions and teach mechanics based on guidance I was given. I sketched ideas for different mechanics and puzzles to get feedback on before implementing prototypes in Unity. I documented my design intentions in detail with annotated screenshots and commented playthrough videos so the team would be able to understand my work without me.</p>
+  <p>As the <i><b>only level designer</b></i> on the team, I worked on 4 levels to <i><b>convey emotions and teach mechanics</b></i>. I sketched ideas for different mechanics and puzzles to get feedback on before implementing prototypes in Unity. I <i><b>documented my design intent</b></i> in detail with annotated screenshots and commented playthrough videos.</p>
 
   <p><b>Timeline:</b> Jun - Sep 2023 (Level Design Contractor)</p>
 
@@ -26,7 +26,7 @@ const ScribbleRushSpan =
 <span>
   A party game that has you draw a growingly complex character in thirty seconds, developed with a team of 4. After finishing a drawing, the player can check out a gallery to see what other people drew!
 
-  <p>Our goal was to make a silly, lighthearted game. I designed simple UI layouts intended to give off a playful, hand-drawn vibe to keep the emphasis on the player's drawing. It was also important to our team to make our game accessible to people with different kinds of color vision deficiency, so I made sure that colors would be distinct for people without normal color vision when developing the <a href="https://davidmathlogic.com/colorblind/#%23DC267F-%23FF6000-%23FFB000-%2343B368-%23527EF1-%235A3FE0-%23FFFFFF-%238C8C8C-%23000000">color palette</a>.</p>
+  <p>Our goal was to make a silly, lighthearted game. I designed simple UI layouts with a playful, hand-drawn vibe and developed an <a href="https://davidmathlogic.com/colorblind/#%23DC267F-%23FF6000-%23FFB000-%2343B368-%23527EF1-%235A3FE0-%23FFFFFF-%238C8C8C-%23000000">accessible color palette</a>.</p>
 
   <p><b>Timeline:</b> October 2022 (Ludum Dare 51 72 Hour Jam), November 2022 (polish for Independent Games Festival)</p>
 
@@ -45,9 +45,9 @@ const DebuggerSpan =
 
   <p>My goal with the game was to convey the idea that something is wrong with the world and the player must fix it (<a href="https://docs.google.com/presentation/d/1Q8mzyRM9JT42bwIH9Ay7DveNa4E0zYMxOa9RfQx3rSw/edit#slide=id.g10a79796d10_7_11">pitch deck for initial idea</a>). I directed an interdisciplinary team of students across multiple disciplines, delegating tasks and managing scope so we could have a completed project in May.</p>
 
-  <p>Originally, our color scheme was too bright and cheerful, so I asked the art team to desaturate and darken it. Additionally, to push the idea that catching bugs is "fixing" the world, I put forth the idea of having music transition from sounding "glitchy" to "pure" as the player catches more bugs.</p>
+  <p>I needed to make sure all aspects of development aligned with the overall creative vision. For example, the original color scheme was very bright and saturated, giving off the impression of a happy forest, so I asked the art team to darken and desaturate the colors for a darker mood. To further push the idea that catching bugs is "fixing" the world, I decided that the music should transition from sounding "glitchy" to "pure" as the player catches more bugs.</p>
   
-  <p>I wanted everyone to feel like they contributed something meaningful to the project and that they could feel proud of their work. I'm happy to say that one of my team members told me the project was the most fun they had all school year, and a subset of the team was excited enough about the project that they wanted to polish it up more the following summer!</p>
+  <p>One of my team members told me the project was the most fun they had all school year, and a subset of the team was excited enough about the project that they wanted to polish it up more the following summer!</p>
 
   <p><b>Timeline:</b> Jan - May 2022 (main development), Jun - Sep 2022 (QOL updates)</p>
 
@@ -62,11 +62,11 @@ const SMLevelSpan =
 <span>
   A 2D platformer level designed to teach the player how to wall jump, inspired by Super Mario and developed solo. Platforms/walls/floor are grey and lava (which kills the player) is red. The player is the white square at the beginning, and the goal is to reach the yellow square at the end.
 
-  <p>The first third of my level has player jump on platforms in a way that emulates the timing and distance required for wall jumping, with the platforms decreasing in size until the player can only progress by wall jumping. The idea is that the player has that jump pattern in their head by the time they have to wall jump and naturally figure out how to wall jump.</p>
+  <p>(1) The player learns to wall jump through repetition of the same motion across shortening platforms.</p>
 
-  <p>The middle third of my level allows the player to make a choice: utilize the newly learned wall jump to take the safer path above, or take the path below (more precarious, but does not require wall jumping). This is intended to encourage the player to use what they just learned.</p>
+  <p>(2) The player is encouraged to utilize their newly-acquired wall jump knowledge to take the safer path up top rather than risk dying to lava.</p>
 
-  <p>Regardless of the player's previous choice, they will see they have no choice but to wall jump to reach the yellow square to complete the level. The area before the last lava pit has walls that the player can practice wall jumping on safely before taking on the final challenge.</p>
+  <p>(3) The player can practice wall jumping safely before taking on the final challenge.</p>
 
   <p><b>Timeline:</b> Jun 2022 (1 week)</p>
 
@@ -82,9 +82,9 @@ const SMLevelAlt = "A platformer level design with grey solid tiles and red lava
 
 const GrabbSpan =
 <span>
-  A 2D aim-and-shoot game featuring a hungry chameleon, developed solo.
+  A 2D aim-and-shoot game featuring a hungry chameleon, developed solo. To win and see a chameleon fun fact, the player must catch and eat 30 bugs in 30 seconds.
 
-  <p>I wanted to try my hand at making a game entirely by myself, so I set a time limit for myself and scoped a simple game accordingly: flies spawn at random heights and enter from the right side of the screen, and the player must catch and eat 30 bugs in 30 seconds.  Like a real chameleon, the player must wait for the tongue to retract entirely before trying to catch a bug again, which adds friction to the gameplay. To match the frenetic nature of the gameplay, I composed a simple melody that speeds up as the time limit approaches. Since I drew both a win and lose screen, I wanted to make sure players couldn't win too easily, though from feedback I received, the game is a bit too difficult.</p>
+  <p>I wanted to try my hand at making a game entirely by myself, so I set a time limit for myself and scoped a simple game accordingly.  Like a real chameleon, the player must wait for the tongue to retract entirely before trying to catch a bug again, which adds friction to the gameplay. To match the frenetic gameplay, I composed a simple melody that speeds up as the time limit approaches.</p>
 
   <p><b>Timeline:</b> Dec 2021 (10 days)</p>
 
