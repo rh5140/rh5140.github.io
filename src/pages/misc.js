4a03c99fdc8f65588d51805e9601c4a30ebdceb5
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2024-03-24 17:08:13 -0700

    add cs 174 stuff to misc

--- src/pages/misc.js
+++ src/pages/misc.js
@@ -1,17 +1,15 @@
 import * as React from 'react'
 import InfoCard from '../components/info-card'
 import Layout from '../components/layout'
+import RaybladesGif from '../images/174a.gif'
+import CorgoGif from '../images/174c.gif'
 import FizzBuzzGif from '../images/FizzBuzz.gif'
 import NebulosusGif from '../images/nebulosus.gif'
 import PkmnGif from '../images/pkmn_art.gif'
 
 const NebulosusSpan =
 <span>
-  A VR experience in which you explore a dreamy world full of sleeping animals, developed solo. Keep a respectful distance from the animals -- they'll vanish into clouds if you get too close!
-
-  <p>This project was developed for <a href="https://classes.dma.ucla.edu/Winter24/172/">Design | Media Arts 172: Virtual Reality and Worldbuilding</a> at UCLA, taught by Jenna Caravello.</p>
-
-  <p>This was my first time working in 3D, and my first time working in VR. I designed, drew, and modeled everything in the world. I used Unity's Terrain system to raise/lower the terrain, change the terrain textures, and paint on grass and trees. <a href="https://www.linkedin.com/in/alvin-liu-b183b31b9">Alvin Liu</a>, my friend and creative collaborator, provided the music for my project.</p>
+  A VR experience in which you explore a dreamy world full of sleeping animals, developed solo with music by <a href="https://www.linkedin.com/in/alvin-liu-b183b31b9">Alvin Liu</a>, my friend and creative collaborator. This project was developed for <a href="https://classes.dma.ucla.edu/Winter24/172/">Design | Media Arts 172: Virtual Reality and Worldbuilding</a> at UCLA, taught by Professor Jenna Caravello and TA Antigoni Tsagkaropoulou. Keep a respectful distance from the animals -- they'll vanish into clouds if you get too close!
   
   <p><b>Timeline:</b> Jan - March 2024 (10 weeks)</p>
 
@@ -24,9 +22,7 @@ const NebulosusAlt = "This gif moves around in a pink, cloudy world. There is a 
 
 const FizzBuzzSpan =
 <span>
-  A character with abilities designed for a group board game.
-
-  <p>This project was developed for <a href="https://classes.dma.ucla.edu/Winter24/157/">Design | Media Arts 157: Game Design</a> at UCLA, taught by Sam Malabre.</p>
+  A character with abilities designed for a group board game, developed solo for <a href="https://classes.dma.ucla.edu/Winter24/157/">Design | Media Arts 157: Game Design</a> at UCLA, taught by Professor Sam Malabre and TA TJ Shin.
 
   <p>This gecko thinks he's gifted with magical powers. Nobody has the heart to tell him the truth of his mundanity, so they play along with his antics. When Fizz Buzz falls below an energy threshold, he loses his wizard hat, and with it, the ability to cast spells (wizard cards). Fizz Buzz's spell cost can be decreased if there are character adjacent to him (believers) helping to convince him of his wizardry. The energy, believer, and threshold tokens are placed on the character sheet to track their respective quantities. The wizard hat can be removed from the character piece to show that Fizz Buzz can't cast spells.</p>
   
@@ -50,16 +46,46 @@ const ArtSpan =
 
 const ArtAlt = "A gif that swaps between three frames. The first frame is a digital illustration of an Alolan Exeggutor designed as a Monster Hunter monster, formatted like a page out of a book. The second is a digital painting of a Flareon curled around a Torchic, sleeping on a pile of blankets. There is a heart made of fire above them. The final frame contains 3 digital drawings in a polygonal style. The one on the left is a shiny Rayquaza on a black background. The one on the top right is a Krokorok with sunglasses, arms crossed under the desert sun. The one on the bottom right is an Empoleon pointing at the viewer with a wing, with a background of geometric ice shapes."
 
+const RaybladesSpan = 
+<span>
+  This Beyblades simulation was developed for Computer Science 174A: Introduction to Computer Graphics, taught by Professor Ashish Law and TA Chenda Duan, with two other students.
+
+  <p><b>Timeline:</b> March 2023 (2.5 weeks)</p>
+
+  <p><b>Tools:</b> JavaScript, WebGL, <a href="https://github.com/niyantn/tiny-graphics-js">tiny-graphics.js</a></p>
+
+  <p>Run it in your browser on this <a href="https://rh5140.github.io/174a-beyblades/">webpage</a> (sound on!) and check out the <a href="https://github.com/rh5140/174a-beyblades">GitHub repo</a>!</p>
+</span>
+
+const RaybladesAlt = "A gif of two Beyblades spinning around on a marble platform with a background of spinning fire."
+
+const CorgoSpan = 
+<span>
+  This animation was developed for Computer Science 174C: Computer Animation, taught by Professor Demetri Terzopoulos and TA Zhi Li, with three other students.
+
+  <p><b>Timeline:</b> February - March 2024 (5 weeks)</p>
+
+  <p><b>Tools:</b> JavaScript, WebGL, <a href="https://github.com/niyantn/tiny-graphics-js">tiny-graphics.js</a></p>
+
+  <p>Run it in your browser on this <a href="https://rh5140.github.io/174c-corgo/">webpage</a> (sound on!) and check out the <a href="https://github.com/rh5140/174c-corgo">GitHub repo</a>! The README has a breakdown of who did what work in the project.</p>
+</span>
+
+const CorgoAlt = "A gif of a frog jumping on a mushroom in an environment of red mushrooms and frogs. It zooms in on the frog, then zooms out to see a corgi is running circles around the frog."
+
 const MiscPage = () => {
   return (
     <Layout pageTitle="Miscellaneous">
       <p>Here's some other stuff I've done and want to share!</p>
       <p></p>
-      <InfoCard image={FizzBuzzGif} alt={FizzBuzzAlt} text={FizzBuzzSpan} title="Fizz Buzz the Wizard Lizard"></InfoCard>
-      <p></p>
       <InfoCard image={PkmnGif} alt={ArtAlt} text={ArtSpan} title="radishark art"></InfoCard>
       <p></p>
+      <InfoCard image={FizzBuzzGif} alt={FizzBuzzAlt} text={FizzBuzzSpan} title="Fizz Buzz the Wizard Lizard"></InfoCard>
+      <p></p>
       <InfoCard image={NebulosusGif} alt={NebulosusAlt} text={NebulosusSpan} title="Nebulosus Reverie"></InfoCard>
+      <p></p>
+      <InfoCard image={CorgoGif} alt={CorgoAlt} text={CorgoSpan} title="Corgo in Wonderland"></InfoCard>
+      <p></p>
+      <InfoCard image={RaybladesGif} alt={RaybladesAlt} text={RaybladesSpan} title="Beyblades"></InfoCard>
     </Layout>
   )
 }
